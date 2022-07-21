cecf7225d417ec2df0b1b1aec678870e053b0b8b
sameeranjan22 <89986653+sameeranjan22@users.noreply.github.com>
2022-07-21 12:46:11 +0530

    Update stripe.js

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
           enabled: true
         },
         shipping_options: [
-          { shipping_rate: 'shr_1LAEPOSHSpLZscfeeEWXTVCP'},
+          { shipping_rate: 'shr_1L9sFdSHSpLZscfe5JScDgIJ'},
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
