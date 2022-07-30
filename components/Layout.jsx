71457179f8c7ccd134409090dc60858488e343a8
sameeranjan22 <89986653+sameeranjan22@users.noreply.github.com>
2022-07-30 12:19:09 +0530

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import Head from 'next/head';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import dbConnect from "../utils/dbConnect";
+
+const connect = () => {
+    dbConnect();
+  }
 
 function Layout({children}) {
   return (
     <div className='layout'>
+        {connect}
         <Head>
             <title>
                 Shop | S P Solutions Point Pvt. Ltd.
